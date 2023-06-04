446b85d89dab44a22436408fcebaecc9ee708709
Segundo Paladines <segundopaladines@gmail.com>
2023-06-04 11:26:06 -0500

    Añadiendo plantilla de adminlte

---
+++ resources/views/index.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+
+@section('title', 'Taller 2')
+
+@section('content_header')
+    <h1>Taller 2</h1>
+@stop
+
+@section('content')
+    <p>Hola Gente.</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
